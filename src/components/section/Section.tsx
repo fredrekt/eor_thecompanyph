582d6d4460d0a2c1300938a27569af776871c293
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-24 17:36:31 +0800

    init design

---
+++ src/components/section/Section.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface SectionProps {
+    className: string;
+    children: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({
+    className,
+    children
+}) => {
+  return (
+    <section className={`sectionContainer ${className}`}>
+        {children}
+    </section>
+  )
+}
+
+export default Section
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Homepage.scss';
 import Layout from '../../layout/Layout';
+import Section from '../../components/section/Section';
+import { Typography } from 'antd';
+import heroImage from '../../assets/images/hero.png'
 
 const Homepage: React.FC = () => {
 	return (
 		<Layout className="homepageContainer">
-			<h1>hello there</h1>
+			<Section className='heroBanner'>
+                <img className='heroImage' src={heroImage} alt="hero image" />
+                <Typography.Title>
+                    Expand faster in the <span className='underline'>philippines</span>
+                </Typography.Title>
+            </Section>
 		</Layout>
 	);
 };
