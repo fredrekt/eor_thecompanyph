cc0dd263b72367ab6efef2be063dc7c3de070019
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-25 14:39:54 +0800

    init components (blog & feature cards)

---
+++ src/components/blogCard/BlogCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BlogCard.scss'
+
+interface BlogCardProps {
+
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({
+    
+}) => {
+  return (
+    <div>BlogCard</div>
+  )
+}
+
+export default BlogCard
---
+++ src/components/featureCard/FeatureCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './FeatureCard.scss'
+
+interface FeatureCardProps {
+
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+    
+}) => {
+  return (
+    <div>FeatureCard</div>
+  )
+}
+
+export default FeatureCard
