0afbe20d4d2cd7238a21f5833c9d776fe9bdb732
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-26 08:52:21 +0800

    added theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: '#3F4726',
+					fontFamily: 'CodecPro',
+				}
+			}}
+		>
 		<Router>
 			<App />
 		</Router>
+		</ConfigProvider>
 	</React.StrictMode>
 );
 
