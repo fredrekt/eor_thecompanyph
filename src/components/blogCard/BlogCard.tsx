415bc7a8ef391abbc111b973d5a093721222e47a
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-26 08:35:55 +0800

    init design

--- src/components/blogCard/BlogCard.tsx
+++ src/components/blogCard/BlogCard.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import './BlogCard.scss'
+import React from 'react';
+import './BlogCard.scss';
+import { Button, Typography } from 'antd';
 
 interface BlogCardProps {
-
+	title: string;
+	previewImgUrl: string;
+	link: string;
 }
 
-const BlogCard: React.FC<BlogCardProps> = ({
-    
-}) => {
-  return (
-    <div>BlogCard</div>
-  )
-}
+const BlogCard: React.FC<BlogCardProps> = ({ title, previewImgUrl, link }) => {
+	return (
+		<div className="blogCard">
+			<div className="blogCardPreviewImgContainer">
+				<img src={previewImgUrl} alt="blog preview" />
+			</div>
+      <div className="blogCardContent">
+        <Typography.Title className="blogCardTitle" level={4}>
+          {title}
+        </Typography.Title>
+        <Button size='large' shape='round' className="blogCardCta" href={link}>
+          Learn more
+        </Button>
+      </div>
+		</div>
+	);
+};
 
-export default BlogCard
+export default BlogCard;
