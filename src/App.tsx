cc5398ba351bd33b747392d68893756aebf20175
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-29 10:49:44 +0800

    contact form added

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Homepage from './pages/Homepage/Homepage';
 import './App.scss';
 import './fonts/CodecPro-Regular.ttf';
 import './fonts/CodecPro-Italic.ttf';
+import ContactPage from './pages/ContactPage/ContactPage';
 
 const App: React.FC = () => {
 	return (
 		<>
 			<Routes>
 				<Route path="/" index element={<Homepage />} />
+				<Route path="/contact" element={<ContactPage />} />
 			</Routes>
 		</>
 	);
